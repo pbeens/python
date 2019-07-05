c931000ffeed116c6f741b2e159143631be82828
Peter Beens <pbeens@gmail.com>
2019-07-04 20:00:52 -0400

    Added 'DeletePeriodsInFilenames' project

---
+++ DeletePeriodsInFilenames/README.md
@@ -0,0 +1,19 @@
+# README.md
+
+The file `createPeriodFiles.py` is used to create _n_ random files with extra periods embedded in te filename.
+Both the length of the filename and the number of periods can be configured.
+
+### Resources Used
+
+- Reference to create folders:<br>
+https://thispointer.com/how-to-create-a-directory-in-python/
+- Why I used f-strings:<br>
+https://realpython.com/python-f-strings/
+- `makedirs()` vs `mkdir()`:<br>
+https://stackoverflow.com/questions/13819496/what-is-different-between-makedirs-and-mkdir-of-os
+- Using `random.choice()` to generate filenames:<br>
+https://pynative.com/python-random-choice/
+- Why it's best not to use `os.path.join()` and what to use instead:<br>
+https://medium.com/@ageitgey/python-3-quick-tip-the-easy-way-to-deal-with-file-paths-on-windows-mac-and-linux-11a072b58d5f
+- Reference used to insert periods at specific locations:<br>
+https://stackoverflow.com/questions/5254445/add-string-in-a-certain-position-in-python
---
+++ DeletePeriodsInFilenames/createPeriodFiles.py
@@ -0,0 +1,36 @@
+# imports
+import os
+import random
+import string
+from pathlib import Path
+
+# global variables
+num_files = 10  # used when creating files
+
+# create files folder
+dir_name = 'files'
+if not os.path.exists(dir_name):
+    os.makedirs(dir_name)
+    print(f"Directory '{dir_name}' created")
+else:
+    print(f"Directory '{dir_name}' already exists -- not created")
+
+# create files
+num_chars = 10
+num_periods = 4
+for i in range(num_files):
+    filename = ''
+    # random characters
+    for j in range(num_chars):
+        filename += random.choice(string.ascii_letters)
+    # insert random periods
+    for j in range(num_periods):
+        location = random.randint(1, len(filename) - 1) # -1 to ensure period isn't at the end
+        filename = filename[:location] + '.' + filename[location:]
+    # append filename extension
+    filename += '.pdf'
+    # warm fuzzy
+    print(filename)
+    # create the file
+    file_path = Path(dir_name + '/' + filename)
+    open(file_path, 'w').close()  # this immediately closes the open file!
